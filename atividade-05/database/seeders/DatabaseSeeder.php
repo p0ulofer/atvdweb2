e487b7dc89c459c2e525fb38cf9cb7bcd8bcb973
p0ulofer <pfernandesof@gmail.com>
2025-10-30 20:51:13 -0300

    atividade 5.1

---
+++ atividade-05/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
+class DatabaseSeeder extends Seeder
+{
+    
+    public function run()
+{
+    $this->call([
+        CategorySeeder::class,
+        AuthorPublisherBookSeeder::class,
+        UserBorrowingSeeder::class, 
+    ]);
+}
+}
