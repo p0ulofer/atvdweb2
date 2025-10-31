de3024271c6dd4623b482d8d5f0892c239875f6d
p0ulofer <pfernandesof@gmail.com>
2025-10-30 22:03:01 -0300

    atividade 5.5

--- atividade-05/app/Providers/AppServiceProvider.php
+++ atividade-05/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +14,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+    public function boot()
+{
+    Paginator::useBootstrap();
+}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
 }
